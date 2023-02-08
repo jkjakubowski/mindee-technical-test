4f8af3a3a8f92e7a3aa55ad20437515cb6e85863
Jan-Kasper <jk.jakubowski25@gmail.com>
2023-02-08 10:16:18 +0100

    created form and onSubmit function

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
 import React, { FC, useState } from "react";
 import "./App.css";
+import { useFormik } from "formik";
 
 import { TextField, Button } from "@mui/material";
-
 import ItemsTable from "./components/table/Table";
 
 const App: FC = () => {
   const [items, setItems] = useState([{ name: "toto", description: "toto" }]);
 
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+    },
+    onSubmit: (values) => {
+      setItems((items) => [
+        ...items,
+        { name: values.name, description: values.description },
+      ]);
+    },
+  });
+
+
   return (
     <div className="App">
-      <div>
-        <TextField id="name" variant="outlined" label="Item's name"></TextField>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          id="name"
+          variant="outlined"
+          label="Item's name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        ></TextField>
         <TextField
           id="description"
           variant="outlined"
           label="Item's description"
+          value={formik.values.description}
+          onChange={formik.handleChange}
         ></TextField>
-        <Button variant="contained">Submit item</Button>
-      </div>
+        <Button type="submit" variant="contained">
+          Submit item
+        </Button>
+      </form>
       <div>
         <ItemsTable rows={items} />
       </div>
